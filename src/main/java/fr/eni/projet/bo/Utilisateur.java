4db1c2fbbdeffa5e211a78c9e9eb0291959a7a08
barletjb <barletjb.perso@gmail.com>
2025-06-30 15:28:59 +0200

    Add Class BO and BLL

--- src/main/java/fr/eni/projet/bo/Utilisateur.java
+++ src/main/java/fr/eni/projet/bo/Utilisateur.java
@@ -1,7 +1,176 @@
 package fr.eni.projet.bo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Utilisateur {
 
-    private long idUtilisateur;
+    private List<Enchere> encheres;
+    private List<Article> articles;
 
+    private long idUtilisateur;
+    private String pseudo;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String telephone;
+    private String rue;
+    private String codePostal;
+    private String ville;
+    private String motDePasse;
+    private int credit;
+    private boolean administrateur;
+
+
+    public Utilisateur(long idUtilisateur, String pseudo, String nom,
+                       String prenom, String email, String telephone, String rue, String codePostal, String ville,
+                       String motDePasse, int credit, boolean administrateur) {
+        this.encheres = new ArrayList<>();
+        this.articles = new ArrayList<>();
+        this.idUtilisateur = idUtilisateur;
+        this.pseudo = pseudo;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.telephone = telephone;
+        this.rue = rue;
+        this.codePostal = codePostal;
+        this.ville = ville;
+        this.motDePasse = motDePasse;
+        this.credit = credit;
+        this.administrateur = administrateur;
+    }
+
+
+    public List<Enchere> getEncheres() {
+        return encheres;
+    }
+
+    public void setEncheres(List<Enchere> encheres) {
+        this.encheres = encheres;
+    }
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
+
+    public long getIdUtilisateur() {
+        return idUtilisateur;
+    }
+
+    public void setIdUtilisateur(long idUtilisateur) {
+        this.idUtilisateur = idUtilisateur;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getRue() {
+        return rue;
+    }
+
+    public void setRue(String rue) {
+        this.rue = rue;
+    }
+
+    public String getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+
+    public boolean isAdministrateur() {
+        return administrateur;
+    }
+
+    public void setAdministrateur(boolean administrateur) {
+        this.administrateur = administrateur;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Utilisateur{" +
+                "encheres=" + encheres +
+                ", articles=" + articles +
+                ", idUtilisateur=" + idUtilisateur +
+                ", pseudo='" + pseudo + '\'' +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", email='" + email + '\'' +
+                ", telephone='" + telephone + '\'' +
+                ", rue='" + rue + '\'' +
+                ", codePostal='" + codePostal + '\'' +
+                ", ville='" + ville + '\'' +
+                ", credit=" + credit +
+                ", administrateur=" + administrateur +
+                '}';
+    }
 }
