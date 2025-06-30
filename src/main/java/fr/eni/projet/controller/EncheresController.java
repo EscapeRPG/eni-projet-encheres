fdd74f98bef0ac6080a0966e70e39296e73a3102
EscapeRPG <syrphin@syrphin.com>
2025-06-30 15:32:00 +0200

    ajout : index, controller pour afficher l'index et fragment thymeleaf

---
+++ src/main/java/fr/eni/projet/controller/EncheresController.java
@@ -0,0 +1,23 @@
+package fr.eni.projet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class EncheresController {
+	
+//	private EncheresService encheresService;
+//
+//	public EncheresController(EncheresService encheresService) {
+//		this.encheresService = encheresService;
+//	}
+	
+	@GetMapping("/")
+	public String gotoIndex() {
+		return "index";
+	}
+
+	
+
+}
