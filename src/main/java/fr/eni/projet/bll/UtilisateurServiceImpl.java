e9a0d72d8116f4edcd6df5ce8902f096d35fc9d8
0oXiwiXo0 <christophe.rbrto@gmail.com>
2025-07-01 12:17:26 +0200

    update page cpnnexion

--- src/main/java/fr/eni/projet/bll/UtilisateurServiceImpl.java
+++ src/main/java/fr/eni/projet/bll/UtilisateurServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class UtilisateurServiceImpl implements UtilisateurService{
 
-	private UtilisateurDAO utilisateurDAO
+	//private UtilisateurDAO utilisateurDAO;
 
     @Override
     public void creerUtilisateur(Utilisateur utilisateur) {
--- src/main/java/fr/eni/projet/controller/EncheresController.java
+++ src/main/java/fr/eni/projet/controller/EncheresController.java
@@ -25,7 +25,7 @@ public class EncheresController {
 		return "inscription";
 	}
 	
-
+ 
 
 	@GetMapping("/connexion")
 	public String gotoConnexion() {
