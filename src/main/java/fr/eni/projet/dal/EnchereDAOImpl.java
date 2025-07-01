e274bd402d6b8e09ba6dca8faa088054ee745f51
dbernier2025 <damione.bern@gmail.com>
2025-07-01 15:01:44 +0200

    Ajout de DAOImpl enchere, modification de connexionCompte pour demander que le pseudo

--- src/main/java/fr/eni/projet/dal/EnchereDAOImpl.java
+++ src/main/java/fr/eni/projet/dal/EnchereDAOImpl.java
@@ -1,5 +1,39 @@
 package fr.eni.projet.dal;
 
+import fr.eni.projet.bo.Enchere;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import java.util.List;
+
 public class EnchereDAOImpl implements EnchereDAO{
 
+    private final NamedParameterJdbcTemplate jdb;
+    public EnchereDAOImpl(NamedParameterJdbcTemplate jdb) {
+        this.jdb = jdb;
+    }
+
+    @Override
+    public void creerEnchere(Enchere enchere) {
+        String sql ="insert into enchere(dateEnchere,montantEnchere,idArticle,idUtilisateur) "
+                + "values (:dateEnchere, :montantEnchere, :idArticle, :idUtilisateur)";
+        MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+        mapSqlParameterSource.addValue("dateEnchere", enchere.getDateEnchere());
+        mapSqlParameterSource.addValue("montantEnchere", enchere.getMontantEnchere());
+        mapSqlParameterSource.addValue("idArticle", enchere.getEnchereArticle().getIdArticle());
+        mapSqlParameterSource.addValue("idUtilisateur", enchere.getUtilisateur().getIdUtilisateur());
+        jdb.update(sql, mapSqlParameterSource);
+    }
+
+    @Override
+    public List<Enchere> consulterEnchere() {
+        String sql = "select * from enchere";
+        return jdb.query(sql,new BeanPropertyRowMapper<>(Enchere.class));
+    }
+
+    @Override
+    public Enchere afficherEnchere(long idArticle) {
+        return null;
+    }
 }
