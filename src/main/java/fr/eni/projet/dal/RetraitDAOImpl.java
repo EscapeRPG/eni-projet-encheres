dd97b2b93831efa7980fb0a1510f9f8656568a77
dbernier2025 <damione.bern@gmail.com>
2025-07-01 15:22:45 +0200

    Ajout de DAO retrait

--- src/main/java/fr/eni/projet/dal/RetraitDAOImpl.java
+++ src/main/java/fr/eni/projet/dal/RetraitDAOImpl.java
@@ -1,5 +1,34 @@
 package fr.eni.projet.dal;
 
+import fr.eni.projet.bo.Retrait;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
 public class RetraitDAOImpl implements RetraitDAO{
 
+
+    private final NamedParameterJdbcTemplate jdb;
+    public RetraitDAOImpl(NamedParameterJdbcTemplate jdb) {
+        this.jdb = jdb;
+    }
+
+    @Override
+    public Retrait afficherRetrait(long idArticle) {
+        String sql = "SELECT * FROM retrait WHERE idArticle = :idArticle";
+        MapSqlParameterSource paramSource = new MapSqlParameterSource();
+        paramSource.addValue("idArticle", idArticle);
+        return jdb.queryForObject(sql, paramSource, Retrait.class);
+    }
+
+    @Override
+    public void creerRetrait(Retrait retrait) {
+        String sql = "insert into retrait(rue,codePostal,ville,idArticle) "
+                + "values (:rue,:codePostal,:ville,:idArticle)";
+        MapSqlParameterSource paramSource = new MapSqlParameterSource();
+        paramSource.addValue("rue", retrait.getRue());
+        paramSource.addValue("codePostal", retrait.getCodePostal());
+        paramSource.addValue("ville", retrait.getVille());
+        paramSource.addValue("idArticle", retrait.getArticle().getIdArticle());
+        jdb.update(sql, paramSource);
+    }
 }
